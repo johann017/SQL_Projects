51dca7dd98147cc3a06168d99934a9614feaa035
Johann Antisseril <57604319+johann017@users.noreply.github.com>
2021-07-15 15:33:58 -0700

    Update Sign_In_Sign_Up_GUI.py

--- Sign_In_Sign_Up_GUI.py
+++ Sign_In_Sign_Up_GUI.py
@@ -89,9 +89,20 @@ def sign_up_func(event):
                 submit.bind("<Button-1>", sign_up_submit)
                 submit.pack()
 
+#This function handles the event the user clicks the sign in button
 def sign_in_func(event):
+    
+    #Clears the screen
     forget()
+    
+    #This function handles the even a user clicks the submit button.
+    #It checks if the username and the password have any common
+    #SQL commands. If it does, it clears the inputted values and displays
+    #a message. If it doesn't, it will check if the username and passwords 
+    #is in the table of usernames and passwords and displays a message accordingly 
     def sign_in_submit(event):
+        
+        #Variables to be used
         global username
         global password
         username = user_var.get()
@@ -113,27 +124,38 @@ def sign_in_func(event):
         else:
             label_name3.pack_forget()
             label_name4.pack()
+            
+    #This function handles the event a user clicks the clear button.
+    #This will clear the username and password fields.
     def sign_in_clear(event):
         username_entry.delete(0,'end')
         password_entry.delete(0,'end')
         label_name3.pack_forget()
         label_name4.pack_forget()
+        
+    #Draws the sign in page
     for i in range(5):
         window.columnconfigure(i,weight = 5,minsize=75)
         window.rowconfigure(i,weight = 5,minsize=50)
         for j in range(3):
             new_frame = tk.Frame(master=window, bg="Light Blue")
             new_frame.grid(row = i, column=j, padx=5, pady=5)
+            
+            #Username box
             if i == 0 and j == 2:
                 label_name = tk.Label(master= new_frame, text="Username: ")
                 label_name.pack(side="top")
                 username_entry = tk.Entry(master= new_frame,textvariable = user_var, font=('calibre',10,'normal'))
                 username_entry.pack()
+            
+            #Password box
             if i == 1 and j == 2:
                 label_name2 = tk.Label(master= new_frame, text="Password: ")
                 label_name2.pack(side="top")
                 password_entry = tk.Entry(master= new_frame,textvariable = password_var, font=('calibre',10,'normal'))
                 password_entry.pack()
+            
+            #Messages based on if user input was correct
             if i == 3 and j == 2:
                 label_name3 = tk.Label(master= new_frame, text="Successfully Logged In!!")
                 label_name3.pack(side="top")
@@ -141,6 +163,8 @@ def sign_in_func(event):
                 label_name4 = tk.Label(master= new_frame, text="Incorrect username or password")
                 label_name4.pack(side="top")
                 label_name4.pack_forget()
+                
+            #Submit and clear buttons
             if i == 4 and j == 2:
                 submit = tk.Button(master=new_frame, width=10, height=2,text="Submit")
                 submit.bind("<Button-1>", sign_in_submit)
@@ -149,6 +173,7 @@ def sign_in_func(event):
                 clear.bind("<Button-1>", sign_in_clear)
                 clear.pack(side = "left", padx=5, pady=5)
 
+#
 def sign_in_helper(u,p):
     my_cursor.execute("SELECT user,pass FROM Users")
     for x,y in my_cursor:
