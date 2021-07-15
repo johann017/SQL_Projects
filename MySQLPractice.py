92715717d8f56bd05bac620bb8476fce87c7bfa6
Johann Antisseril <57604319+johann017@users.noreply.github.com>
2021-07-15 15:20:02 -0700

    Update MySQLPractice.py

--- MySQLPractice.py
+++ MySQLPractice.py
@@ -15,14 +15,23 @@ my_cursor.execute("CREATE TABLE Testing (name VARCHAR(50), age int)")
 #Array of names to be added to the table
 names = ["Brad", "James", "Ellie", "Margo", "Abe", "Rachel"]
 
-#
+#Creates a query without arguments to be used
 query = "INSERT INTO testing (name, age) VALUES (%s,%s)"
 count = 1
+
+#Goes through the array of names to be added and inserts each name and age
+#into the table
 for name in names:
     my_cursor.execute(query, (name,count))
     count += 1
+
+#Commits the changes to the database
 db.commit()
+
+#Prints all elements in the table to show elements were added
 my_cursor.execute("SELECT * FROM Testing")
 for x in my_cursor:
     print(x)
+   
+#Deletes the table so next execution of this code wont have a table
 my_cursor.execute("DROP TABLE Testing")
